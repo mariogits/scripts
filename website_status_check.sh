f63c464e961b4342c7a726540f0ed246950f3c40
Mario <mariodimovx@gmail.com>
2018-10-16 11:58:32 -0400

    modified website_status_check.sh

--- website_status_check.sh
+++ website_status_check.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Usage
+# Replace the website domain name and the email address in the script.
+# Create a cron job to run the script periodically to check the status of your website.
+# If the site is not accesible, you will receive notifiaction via email. 
 
 WEBSITE="domain.com"
 EMAIL="username@domain.com"
